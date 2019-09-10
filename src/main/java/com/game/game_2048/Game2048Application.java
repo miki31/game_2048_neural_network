4259e779be4047a275c783a77746486dbf8670a6
miki31 <it.misha2016@gmail.com>
2019-09-10 23:00:20 +0300

    Initial commit

---
+++ src/main/java/com/game/game_2048/Game2048Application.java
@@ -0,0 +1,13 @@
+package com.game.game_2048;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Game2048Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Game2048Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/game/game_2048/Game2048ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.game.game_2048;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Game2048ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
